c14ac588bf5407df3fd4f3d5df8a917d5c39b59e
Ann Catherine Jose <ann.jose@gmail.com>
2023-10-20 16:53:03 -0700

    LearnJS course Object destructuring default values

--- learnjs-course/8-objects-advanced.js
+++ learnjs-course/8-objects-advanced.js
@@ -25,3 +25,35 @@ console.log(`user=${user}`);  // user=[object Object]
 // Object.values
 console.log(Object.values(user));   // [1,'John',34]
 console.log(Object.entries(user));  // [ ['id', 1], ['name', 'John'], ['age', 34]]
+
+// Object shorthand
+let age = 34;
+let person = { age };
+console.log('Age: ' + person.age);  // Age: 34
+
+// Object destructuring
+const config = {
+    id: 1, isAdmin: true, theme:
+        { darkMode: true, accessbility: true }
+};
+const id_ = config.id
+const isAdmin_ = config.isAdmin;
+console.log(id_, isAdmin_); // 1 true
+
+const { id, isAdmin } = config;
+console.log(id, isAdmin); // 1 true
+
+// default value
+const config2 = { id: 2 };
+const { isAdmin2 = false } = config;
+console.log('default value: ' + isAdmin2);  // default value: false
+
+// rename value
+const { id: userId } = config;
+console.log('renamed id to userId: ', userId);  // renamed id to userId:  1
+
+// Object concatenation
+const defaultOptions = { tabSize: 4, fontSize: 10 };
+const options = { tabSize: 2 };
+const newObj = { ...defaultOptions, ...options };
+console.log('merged object: ', newObj);    // merged object:  {tabSize: 2, fontSize: 10}
