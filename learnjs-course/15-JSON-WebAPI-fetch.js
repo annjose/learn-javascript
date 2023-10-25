b3367937a650b9b64ec85f0bb2444e3ef6e280e2
Ann Catherine Jose <ann.jose@gmail.com>
2023-10-25 11:14:21 -0700

    LearnJS: More examples of fetch

--- learnjs-course/15-JSON-WebAPI-fetch.js
+++ learnjs-course/15-JSON-WebAPI-fetch.js
@@ -24,6 +24,9 @@ console.log(willItRain('{"now":{"temperature":18,"humidity":"30%","uvIndex":0,"r
 console.log(willItRain('{"now":{"temperature":25,"humidity":"90%","uvIndex":4,"rainExpected":false}}')); // false
 
 // fetch Web API
+
+/* 
+// API 1 - notifications
 const url = "https://jsdemo-3f387-default-rtdb.europe-west1.firebasedatabase.app/notifications/new.json";
 fetch(url)
     .then(response => {
@@ -49,3 +52,55 @@ fetch(url)
         console.log("concise form");
         console.log(data); // {count: 3, message: 'You've got 3 new notifications'}
     }); 
+
+*/
+
+/*
+// API 2 - users array
+const url = "https://jsonplaceholder.typicode.com/users";
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(`request url: ${url}`);
+        console.log(data);
+
+        // log the email of all the users
+        let emails = [];
+        data.forEach(user => {
+            emails.push(user.email);
+        });
+        console.log(emails);
+    });
+*/
+
+/*
+// API 3 - todos 
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        console.log(`title = ${data.title}`);   // delectus aut autem
+    });
+*/
+
+// API 4 - Tweet details (Coding challenge)
+const getTweetDetails = () => {
+    const url = "https://jsdemo-3f387-default-rtdb.europe-west1.firebasedatabase.app/tweet/1080777336298049537.json";
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(JSON.stringify(data));
+            showTweetDetails(data);
+        });
+}
+
+
+function showTweetDetails(tweet) {
+    const authorDetails = tweet.author.details;
+    const fullName = `${authorDetails.firstName} ${authorDetails.lastName}`;
+    console.log(`Author name: ${fullName}`);
+}
+
+getTweetDetails();  // (after delay): Author name: Jad Joubran
