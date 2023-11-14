f46e37521272d60c7c8ffa18928316858ea79cba
Ann Catherine Jose <ann.jose@gmail.com>
2023-11-13 21:12:25 -0800

    ExecJS - More number methods - isSafeInteger

--- execprogram-modernjs-course/execjs-objects-accessors-computed-props-NaN.js
+++ execprogram-modernjs-course/execjs-objects-accessors-computed-props-NaN.js
@@ -29,7 +29,7 @@ const user = {
 const userName1 = user.userName;
 name1 = 'Betty';
 const userName2 = user.userName;
-console.log([userName1, userName2]);['Amir', 'Betty']
+console.log([userName1, userName2]);    // ['Amir', 'Betty']
 
 // there are setters similar to getters
 const user4 = {
@@ -85,3 +85,38 @@ console.log('isNaN(undefined)', isNaN(undefined));          // prints true, but 
 
 console.log('Number.isNaN(undefined - 1)', Number.isNaN(undefined - 1));  // prints true
 console.log('Number.isNaN(undefined)', Number.isNaN(undefined));          // prints false. CORRECT!
+
+// More Number methods
+console.log(Number.isFinite(5));         // true
+console.log(Number.isFinite(Infinity));  // false
+console.log(Number.isFinite(-Infinity)); // false
+console.log(Number.isFinite(NaN));       // false
+console.log(Number.isFinite("test"));    // false
+
+console.log(`Number.MAX_SAFE_INTEGER=${Number.MAX_SAFE_INTEGER}`);   // 9007199254740991
+console.log(`Number.MIN_SAFE_INTEGER=${Number.MIN_SAFE_INTEGER}`);   // -9007199254740991
+console.log(Number.MAX_SAFE_INTEGER === -Number.MIN_SAFE_INTEGER);   // true
+
+// operations outside MAX_SAFE_INTEGER and MIN_SAFE_INTEGER are NOT safe
+console.log(Number.MAX_SAFE_INTEGER + 1);   // 9007199254740992
+console.log(Number.MAX_SAFE_INTEGER + 2);   // 9007199254740992
+console.log(Number.MAX_SAFE_INTEGER + 1 === Number.MAX_SAFE_INTEGER + 2);   // true
+
+// operations between MAX_SAFE_INTEGER and MIN_SAFE_INTEGER are safe
+console.log(Number.MAX_SAFE_INTEGER - 1 === Number.MAX_SAFE_INTEGER);   // false
+
+// IsSafeInteger 
+console.log(Number.isSafeInteger(Number.MAX_SAFE_INTEGER));      // true
+console.log(Number.isSafeInteger(Number.MAX_SAFE_INTEGER + 1));  // false
+console.log(Number.isSafeInteger(Number.MIN_SAFE_INTEGER));      // true
+console.log(Number.isSafeInteger(Number.MIN_SAFE_INTEGER - 1));  // false
+
+const safeIntegerMultiply = (x, y) => {
+    const z = x * y;
+    if (Number.isSafeInteger(z)) {
+        return z;
+    }
+    throw new Error("Product is an unsafe integer");
+}
+console.log(safeIntegerMultiply(10, 20));   // 200
+// console.log(safeIntegerMultiply(Number.MAX_SAFE_INTEGER, 20));  // Uncaught Error Error: Product is an unsafe integer
