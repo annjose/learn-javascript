f5b7deb92ea550ce532e15438e3e223e974df4f0
Ann Catherine Jose <ann.jose@gmail.com>
2023-11-14 05:58:56 -0800

    ExecJS: Function names

--- execprogram-modernjs-course/execjs-classes-function-names.js
+++ execprogram-modernjs-course/execjs-classes-function-names.js
@@ -20,3 +20,27 @@ console.log(people);   // '[{"name":"Amy","age":13},{"name":"Ben","age":40}]'  (
 // Errors
 // const a = Person.canVote(); // Uncaught TypeError TypeError: Person.canVote is not a function
 // const b = Person();     // Uncaught TypeError TypeError: Class constructor Person cannot be invoked without 'new'
+
+// Function Names
+function five() {
+    return 5;
+}
+console.log(five.name); // five
+
+// Anonymous function assigned to a variable
+const two = function () { return 2; }
+console.log(two.name);  // two
+
+// Anonymous function NOT assigned to a variable (the variable `a` here is for the `name` property, not the function.)
+const a = (function () { return 3; }).name;
+console.log("Name of anonymous function: " + a);    // Name of anonymous function: 
+
+// Anoonymous functions assigned to an array, they don't have a name
+const arr = [function () { return 1; }, function () { return 2 }];
+console.log("Names: " + arr[0].name, arr[1].name);  // Names: 
+
+// Arrow functions
+const four = () => { return 4; };
+console.log("Name: " + four.name);  // Name: four
+const arrowName = (() => { }).name;
+console.log("Name: " + arrowName);  // Name: 
